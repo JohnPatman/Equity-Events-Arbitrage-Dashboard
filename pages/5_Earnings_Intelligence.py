d0afc82bf3dcd75e60960fecba8fd0f8274d338e
John Patman <johnwilliampatman1@gmail.com>
2026-05-16 14:10:54 +0100

    Fix: add st.set_page_config() before apply_bloomberg_theme() in Earnings Intelligence page

--- pages/5_Earnings_Intelligence.py
+++ pages/5_Earnings_Intelligence.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_page_config(page_title="Earnings Intelligence", layout="wide")
 from modules.theme import apply_bloomberg_theme
 import pandas as pd
 import altair as alt
