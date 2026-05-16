d670f4002c2906a05d961e4ac209573f2bf64a4a
John Patman <johnwilliampatman1@gmail.com>
2026-05-16 14:11:24 +0100

    Fix: add st.set_page_config() before apply_bloomberg_theme() in Upcoming UK Dividends page

--- pages/6_Upcoming Popular UK Dividends.py
+++ pages/6_Upcoming Popular UK Dividends.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.set_page_config(page_title="Upcoming Popular UK Dividends", layout="wide")
 from modules.theme import apply_bloomberg_theme
 apply_bloomberg_theme()
 import pandas as pd
